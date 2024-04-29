32b61229b0372230bb118f1fcaf35a8f42c341a6
Dylan Butzlaff <500146113@fvtc.edu>
2024-04-29 15:16:10 -0500

    laid down groundwork for routes

--- app.js
+++ app.js
@@ -3,6 +3,8 @@
 
 const express = require('express')
 
+const apiRoutes = require('./api-routes')
+
 const { request } = require('http')
 
 const app = express()
@@ -11,7 +13,7 @@ const port = process.env.PORT || 3000
 app.use(express.json())
 app.use(express.static('public'))
 //uncomment when non-static endpoints are added
-//app.use('/api/v1', require('./routes/api-v1'))
+app.use('/api/v1', require('./routes/api-v1'))
 app.use('/', require('./routes/static'))
 
 
--- routes/api-v1.js
+++ routes/api-v1.js
@@ -1,3 +1,105 @@
+const router = require('express').Router()
+const { MongoClient, ObjectId } = require('mongodb')
+
+const url = process.env.MONGODB_URI || require('./secret/mongodb.json').url
+const client = new MongoClient(url)
+
+const getCollection = async (dbName, collectionName) => {
+	await client.connect()
+	return client.db(dbName).collection(collectionName)
+}
+
+// GET /api/menu id, name, description
+
+router.get('/api/menu', async (request, response) => {
+	const collection = await getCollection('movie-api', 'movies')
+	const movies = await collection.find().toArray()
+	response.send(movies)
+})
+
+// POST /api/menu name, description, price
+
+router.post('/api/menu', async (request, response) => {
+	const{ body } = request
+	const { name, description, price } = body
+	const movie = { name, description, price }
+
+	const collection = await getCollection('movie-api', 'movies')
+	const result = await collection.insertOne(movie)
+	response.send(result)
+})
 
 
-//uncomment line 14 in app.js when non-static endpoints are added
+// PUT /api/menu/:id name, description, price
+
+router.put('/api/menu/:id', async (request, response) => {
+	const{ body, params } = request
+	const { id } = params
+	const { name, description, price } = body
+	const movie = { name, description, price }
+
+	const collection = await getCollection('movie-api', 'movies')
+	const result = await collection.updateOne({ _id: new ObjectId(id) }, { $set: movie })
+	response.send(result)
+})
+
+// DELETE /api/menu/:id
+
+router.delete('/api/menu/:id', async (request, response) => {
+	const { id } = request.params
+	const collection = await getCollection('movie-api', 'movies')
+	const result = await collection.deleteOne({ _id: new ObjectId(id) })
+	response.send(result)
+})
+
+// GET /api/events id, name
+
+router.get('/api/events', async (request, response) => {
+	const collection = await getCollection('movie-api', 'movies')
+	const movies = await collection.find().toArray()
+	response.send(movies)
+})
+
+// GET /api/events/:id name, location, dates, hours
+
+router.get('/api/events/:id', async (request, response) => {
+	const { id, location, dates, hours } = request.params
+	const collection = await getCollection('movie-api', 'movies')
+	const movie = await collection.findOne({ _id: new ObjectId(id) })
+	response.send(movie)
+})
+
+
+// POST /api/events name, location, dates, hours
+
+router.post('/api/events', async (request, response) => {
+	const{ body } = request
+	const { name, location, dates, hours } = body
+	const movie = { name, location, dates, hours }
+
+	const collection = await getCollection('movie-api', 'movies')
+	const result = await collection.insertOne(movie)
+	response.send(result)
+})
+
+// PUT /api/events/:id name, location, dates, hours
+
+router.put('/api/events/:id', async (request, response) => {
+	const{ body, params } = request
+	const { id } = params
+	const { name, location, dates, hours } = body
+	const movie = { name, location, dates, hours }
+
+	const collection = await getCollection('movie-api', 'movies')
+	const result = await collection.updateOne({ _id: new ObjectId(id) }, { $set: movie })
+	response.send(result)
+})
+
+// DELETE /api/events/:id
+
+router.delete('/api/events/:id', async (request, response) => {
+	const { id } = request.params
+	const collection = await getCollection('movie-api', 'movies')
+	const result = await collection.deleteOne({ _id: new ObjectId(id) })
+	response.send(result)
+})
